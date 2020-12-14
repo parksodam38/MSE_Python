28cca540b866aeebb11e9d1d6d72cc004e976bba
sodam <pinksodam28@naver.com>
2020-12-14 23:00:47 +0900

    added python file

---
+++ ex080.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+data = tuple(range(2, 100, 2))  # data 라는 변수에 tuple로 데이터 저장
+print(data)                     # range 함수 = (처음 숫자 지정, 마지막 숫자 지정 -> 마지막 숫자는 포함 안됨, 구간 지정)
+
+
+# In[ ]:
+
+
+
+
