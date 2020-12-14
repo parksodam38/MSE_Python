0e97452197b6432938e62636358a190b27840dd9
sodam <pinksodam28@naver.com>
2020-12-14 23:10:53 +0900

    added python file

---
+++ ex230.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+def my_print(a, b):      
+    print("왼쪽:", a)    # a 변수 왼쪽에 "왼쪽:" 출력
+    print("오른쪽:", b)  # b 변수 왼쪽에 "오른쪽:" 출력
+    
+my_print(b=100, a=200)   # a 변수에 200, b 변수에 100을 지정하였으므로 왼쪽: 200 오른쪽: 100으로 출력됨 
+
