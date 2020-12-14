e33164c785439ed4c6902d251f0a6959f99dc586
sodam <pinksodam28@naver.com>
2020-12-14 22:55:41 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+string = 'abcd'          # string 변수에 'abcd' 지정
+string.replace('b', 'B') # replace 메서드를 사용하면 변경된 새로운 문자열 객체를 리턴해준다. 
+print(string)            # 하지만 문자열은 변경할 수 없는 자료형이기 때문에 abcd가 그대로 출력된다.
+
+
+# In[ ]:
+
+
+
+
