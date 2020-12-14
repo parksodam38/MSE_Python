aa04567c36fe4dce885102afb916e5943f79266d
sodam <pinksodam28@naver.com>
2020-12-14 23:00:02 +0900

    added python file

---
+++ ex070.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# 방법 1
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()    # sort() 메서드는 데이터를 오름차순으로 정렬함
+print(data)
+
+
+# In[10]:
+
+
+# 방법 2
+data = [2, 4, 3, 1, 5, 10, 9]
+data2 = sorted(data)   # data2 라는 변수를 지정, sorted(data) 메서드로 데이터를 오름차순으로 정렬함
+print(data2)
+
+
+# In[ ]:
+
+
+
+
