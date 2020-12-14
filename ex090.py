fe57d64cdd3c4939934abcf2d6cb8400f11954ee
sodam <pinksodam28@naver.com>
2020-12-14 23:01:34 +0900

    added python file

---
+++ ex090.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+icecream ={'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']                    # icecream 딕셔너리에 없는 키 '누가바'를 사용해서 인덱싱하면 에러가 발생함
+Traceback (most recent call last):
+    File "<pyshell#69>", line 1, in <module>
+        icecream['누가바']
+KeyError: '누가바'
+
+
+# In[ ]:
+
+
+
+
