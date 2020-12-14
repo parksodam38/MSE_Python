582f25cc43dcea88d64cc413afbcdf73da56fa9a
sodam <pinksodam28@naver.com>
2020-12-14 23:11:37 +0900

    added python file

---
+++ ex250.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import numpy     # numpy 모듈 불러오기
+for i in numpy.arange(0, 5.1, 0.1):   # arange 함수 = (초기 숫자, 마지막 숫자 -> 포함 안됨, 구간 지정)
+    print(i)
+
