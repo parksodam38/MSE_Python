6a6438938555f0d563a5367a39476a394b7142b2
sodam <pinksodam28@naver.com>
2020-12-14 22:57:32 +0900

    added python file

---
+++ ex050.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+data = "039490    "   # rstrip() 메서드를 사용하면 오른쪽 공백이 제거된 새로운 문자열 객체가 반환됨
+data = data.rstrip()  # data라는 변수에 오른쪽 공백이 제거된 새로운 data를 바인딩
+print(data)           # 기존의 공백이 포함된 문자열은 메모리에서 자동으로 삭제됨
+
+
+# In[ ]:
+
+
+
+
