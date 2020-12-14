a6b61d4991ba830cad5249acf454c7a17d4e6682
sodam <pinksodam28@naver.com>
2020-12-14 22:58:31 +0900

    added python file

---
+++ ex060.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+nums = [1, 2, 3, 4, 5]           # nums라는 리스트에 변수 1, 2, 3, 4, 5 지정
+average = sum(nums) / len(nums)  # average라는 변수 지정, 리스트의 총합 / 리스트 변수의 갯수 
+print(average)
+
+
+# In[ ]:
+
+
+
+
