bfc39f37beeaa28c1c8050e00abe441bf54e7a26
sodam <pinksodam28@naver.com>
2020-12-14 22:56:25 +0900

    added python file

---
+++ ex040.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+data = "  삼성전자  "  # strip() 메서드를 사용하면 좌우의 공백 제거 가능
+data1 = data.strip()   # 새로운 data1 변수에 공백이 제거된 data 지정
+print(data1)      
+
+
+# In[ ]:
+
+
+
+
